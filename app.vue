a89397bc1ff69d4743d7dc456e004b2b604ca435
Bill Cathey <billcathey@hotmail.com>
2022-08-16 23:30:08 -0400

    Add conditional class to buttons, define state object type, define string types for keys

--- app.vue
+++ app.vue
@@ -1,3 +1,30 @@
+<script setup lang='ts'>
+enum Gender {
+  FEMALE = 'Female',
+  MALE = 'Male',
+  UNISEX = 'Unisex',
+}
+enum Popularity {
+  TRENDY = 'Trendy',
+  UNIQUE = 'Unique',
+}
+enum Length {
+  LONG = 'Long',
+  ALL = 'All',
+  SHORT = 'Short',
+}
+interface OptionsState {
+  gender: Gender;
+  popularity: Popularity;
+  length: Length;
+}
+const options = reactive<OptionsState>({
+  gender: Gender.FEMALE,
+  popularity: Popularity.TRENDY,
+  length: Length.SHORT,
+})
+</script>
+
 <template>
   <div class="container">
     <h1>Name Generator</h1>
@@ -6,24 +33,24 @@
       <div class="option-container">
         <h4>1) Choose a gender</h4>
         <div class="option-buttons">
-          <button class="option option-left">Male</button>
-          <button class="option">Unisex</button>
-          <button class="option option-right">Female</button>
+          <button :class="options.gender === Gender.MALE && 'option-active'" class="option option-left">Male</button>
+          <button :class="options.gender === Gender.UNISEX && 'option-active'" class="option">Unisex</button>
+          <button :class="options.gender === Gender.FEMALE && 'option-active'" class="option option-right">Female</button>
         </div>
       </div>
       <div class="option-container">
         <h4>2) Choose the name's popularity</h4>
         <div class="option-buttons">
-          <button class="option option-left">Trendy</button>
-          <button class="option option-right">Unique</button>
+          <button :class="options.popularity === Popularity.TRENDY && 'option-active'" class="option option-left">Trendy</button>
+          <button :class="options.popularity === Popularity.UNIQUE && 'option-active'" class="option option-right">Unique</button>
         </div>
       </div>
       <div class="option-container">
         <h4>3) Choose the name's length</h4>
         <div class="option-buttons">
-          <button class="option option-left">Long</button>
-          <button class="option">All</button>
-          <button class="option option-right">Short</button>
+          <button :class="options.length === Length.LONG && 'option-active'" class="option option-left">Long</button>
+          <button :class="options.length === Length.ALL && 'option-active'" class="option">All</button>
+          <button :class="options.length === Length.SHORT && 'option-active'" class="option option-right">Short</button>
         </div>
       </div>
     </div>
@@ -67,5 +94,9 @@ h1 {font-size: 3rem;}
 }
 .option-right {
   border-radius: 0 1rem 1rem 0;
+}
+.option-active {
+  background-color: hsl(360, 90%, 70%);
+  color: white;
 }
 </style>
