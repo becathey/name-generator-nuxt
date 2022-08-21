bfc13acfd4191eeb2cecca16e7daeb0da186dd47
Bill Cathey <billcathey@hotmail.com>
2022-08-20 22:54:27 -0400

    Add Option component

---
+++ components/Option.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="option-container">
+        <h4>3) Choose the name's length</h4>
+        <div class="option-buttons">
+          <button 
+            :class="options.length === Length.LONG && 'option-active'" 
+            class="option option-left"
+            @click="options.length = Length.LONG"
+          >Long
+          </button>
+          <button 
+            :class="options.length === Length.ALL && 'option-active'" 
+            class="option"
+            @click="options.length = Length.ALL"
+          >All
+          </button>
+          <button 
+            :class="options.length === Length.SHORT && 'option-active'" 
+            class="option option-right"
+            @click="options.length = Length.SHORT"
+          >Short
+          </button>
+        </div>
+      </div>
+</template>
